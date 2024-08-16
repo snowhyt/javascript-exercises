884019b1b7ca0fe1c3862fd9a64b6c9a72ae0d9a
oliverDev <oliverglorioso.44@gmail.com>
2024-08-17 03:49:14 +0800

    Answered Test_01 and Test_02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,20 @@
-const repeatString = function() {
+const repeatString = function(print, counter ) {
+ 
+let output ="";
+
+if(counter >= 0){
+    for(let i = 1; i <= counter; i++ ){
+        output += print;
+    }
+    return output;
+}
+else{
+    output = "ERROR";
+    return output;
+}
+
+
+
 
 };
 
