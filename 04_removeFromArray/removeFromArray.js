d55682ecd978eb2d4432e2325518bca879fe017f
oliverDev <oliverglorioso.44@gmail.com>
2024-08-18 03:51:39 +0800

    04_removeFromArray Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arraySeries, num) {
+    let arrayOrig= [''];
+    let arrayRemove = arraySeries.slice(num);
+    
+    return arrayRemove
 };
 
 // Do not edit below this line
