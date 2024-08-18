bc267c1b184e8c5f421885cbc023f144701e468f
oliverDev <oliverglorioso.44@gmail.com>
2024-08-18 20:27:05 +0800

    7 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  
+  let celsiusConversion = (fahrenheit-32) * 5/9;
+
+ const formattedNum = toFixed(1).Math.round(celsiusConversion * 100)/100;
+  
+  return formattedNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let celsiusFahrenheit = celsius * (9/5);
+  const formattedNum = toFixed(1).Math.round(celsiusFahrenheit * 100)/100;
+  return formattedNum;
 };
 
-// Do not edit below this line
+// Do not edit below this line °F = °C × (9/5) + 32 C = (°F - 32) × 5/9
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
