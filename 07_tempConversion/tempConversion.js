6b6a362524fb22295d93c031b68bd07f36c0a609
snowhyt <109935240+snowhyt@users.noreply.github.com>
2024-08-26 17:16:06 +0800

    done calculator (add, sum, minus, multiply, power)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,16 +1,11 @@
 const convertToCelsius = function(fahrenheit) {
   
-  let celsiusConversion = (fahrenheit-32) * 5/9;
+  return Math.round((fahrenheit-32) * (5/9) * 10) /10;
 
- const formattedNum = toFixed(1).Math.round(celsiusConversion * 100)/100;
-  
-  return formattedNum;
 };
 
 const convertToFahrenheit = function(celsius) {
-  let celsiusFahrenheit = celsius * (9/5);
-  const formattedNum = toFixed(1).Math.round(celsiusFahrenheit * 100)/100;
-  return formattedNum;
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line °F = °C × (9/5) + 32 C = (°F - 32) × 5/9
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
+//addition
+const add = function(a, b) {
+	return a + b;
+};
+
+//substract
+const subtract = function(a, b) {
+	return a - b;
+};
+
+//array
+const sum = function(array) {
+  const initialValue = 0;
+  let sumInitial = initialValue;
+
+  for(let i =0; i < array.length; i++){
+    sumInitial += array[i];
+  }
+  return sumInitial;
+
+};
+
+//multiply
+const multiply = function(array) {
+  const initialValue = 1;
+  let prodInitial = initialValue;
+
+  for(let i =0; i < array.length; i++){
+    prodInitial *= array[i];
+  }
+  return prodInitial;
+};
+
+
+//power
+const power = function(a, b) {
+  let power = 1;
+  for(let i = 0; i < b; i++){
+    power *=  a;
+  }
+  return power;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num1) {
+	if(num1 == 0 || num1 == 1){
+    return 1;
+  }
+  
+  else{
+    return num1 * factorial(n-1);
+  }
 };
 
 // Do not edit below this line
