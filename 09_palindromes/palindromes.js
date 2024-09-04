2a410746ead8a5600dbbdea1eff9e15a0b3d18dd
snowhyt <oliverglorioso.44@gmail.com>
2024-09-05 00:22:07 +0800

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+const palindromes = function (str) {
+//     str.replace(/\s/g, "");
+//     let j = str.length-1;
+
+//     for(let i = 0; i <= str.length/2; i++){
+    
+//         if(j != i){
+//             return true;
+//         }
+//         j--;
+//     }
+//     return false;
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const filteredString = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+
+
+    let revString = filteredString.split('').reverse().join('');
+
+    return filteredString === revString;
 
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
