7dc8f2194270aa29a2edb9d7663e6c615bb22282
snowhyt <oliverglorioso.44@gmail.com>
2024-09-13 01:14:57 +0800

    compelete na lahat ng activity

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,45 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+
+
+let count = 0;
+
+if(typeof countArg !== 'number' ){
+    count = parseInt(countArg);
+} else{
+    count = countArg;
+}
+
+if(count < 0) return 'OOPS';
+if(count == 0) return 0;
+
+
+
+let prevArray = [0,1];
+
+for(let i = 2; i <= count; i++){
+    prevArray[i] = prevArray[i-1] + prevArray[i-2];
+}
+
+return prevArray[count];
+
+
+
+// let firstPrev = 1;
+// let secondPrev = 0;
+
+// for(let i = 2; i <= count; i++){
+//     let current = firstPrev + secondPrev;
+//     secondPrev = firstPrev;
+//     firstPrev = current;
+// }
+
+return firstPrev;
+
+
+
+
+
+
 
 };
 
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,17 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+
+    // let titles = [];
+
+    // for(let i = 0; i < array.length; i++){
+    //        titles.push(array[i].title);
+
+    // }
+
+    // return titles;
+
+
+
+    return array.map((book) => book.title);
 
 };
 
